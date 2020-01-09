e4d61f3d6bceaec513c3e714e2f1b0a900ae7baf
huangxjgit <2250016889@qq.com>
2020-01-09 08:48:04 +0800

    双击返回退出应用

--- app/src/main/java/com/example/diancan/DiancanActivity.java
+++ app/src/main/java/com/example/diancan/DiancanActivity.java
@@ -2,12 +2,36 @@ package com.example.diancan;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.widget.Toast;
 
 public class DiancanActivity extends AppCompatActivity {
 
+    // 用来计算返回键的点击间隔时间
+    private long exitTime = 0;
+    private ShouyeFragment shouyeFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diancan);
+    }
+
+
+
+
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK
+                && event.getAction() == KeyEvent.ACTION_DOWN) {
+            if ((System.currentTimeMillis() - exitTime) > 2000) {
+                //弹出提示，可以有多种方式
+                Toast.makeText(getApplicationContext(), "再按一次退出程序", Toast.LENGTH_SHORT).show();
+                exitTime = System.currentTimeMillis();
+            } else {
+                finish();
+            }
+            return true;
+        }
+
+        return super.onKeyDown(keyCode, event);
     }
 }
