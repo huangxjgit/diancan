b61813e8f4bd35a9300180ba38af8e79c7da3862
huangxjgit <2250016889@qq.com>
2020-01-16 17:50:02 +0800

    地图定位实现

--- app/src/main/java/com/example/diancan/ShouyeFragment.java
+++ app/src/main/java/com/example/diancan/ShouyeFragment.java
@@ -1,15 +1,19 @@
 package com.example.diancan;
 
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.content.res.Resources;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -23,10 +27,20 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.LinearLayout;
 import android.widget.SearchView;
+import android.widget.TextView;
 import android.widget.Toolbar;
 
+import com.baidu.location.BDAbstractLocationListener;
+import com.baidu.location.BDLocation;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+import com.baidu.mapapi.SDKInitializer;
 import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.MapStatusUpdate;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
 import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationData;
+import com.baidu.mapapi.model.LatLng;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -34,10 +48,13 @@ import java.util.List;
 
 public class ShouyeFragment extends Fragment {
     private BaiduMap baiduMap;
+    public LocationClient mLocationClient;
+    private boolean isFirstLocate = true;
     private View view;
     private MapView mapView = null;
     private SearchView searchView;
     private SwipeRefreshLayout swipeRefresh;
+
 
     private Shangjia[]shangjias={new Shangjia("德德塔炸鸡汉堡",R.drawable.bhanbao),new Shangjia("有一家海鲜焖面",R.drawable.bhuajia),
             new Shangjia("黄金炒饭",R.drawable.bchaofan),
@@ -48,10 +65,14 @@ public class ShouyeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        SDKInitializer.initialize(getActivity().getApplicationContext());
         view = inflater.inflate(R.layout.fragment_shouye, container, false);
         mapView = (MapView) view.findViewById(R.id.bmapView);
         baiduMap = mapView.getMap();
         baiduMap.setMyLocationEnabled(true);
+        mLocationClient = new LocationClient(getActivity().getApplicationContext());
+        mLocationClient.registerLocationListener(new MyLocationListener());
+        mapView = (MapView) view.findViewById(R.id.bmapView);
 
         searchView = (SearchView) view.findViewById(R.id.serachview);
         //设置查询提示字符串
@@ -71,9 +92,47 @@ public class ShouyeFragment extends Fragment {
             @Override
             public void onRefresh() {
                 refreshItem();
+                requestLocation();
             }
         });
+
+        List<String> permissionList = new ArrayList<>();
+        if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            permissionList.add(Manifest.permission.ACCESS_FINE_LOCATION);
+        }
+        if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            permissionList.add(Manifest.permission.READ_PHONE_STATE);
+        }
+        if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            permissionList.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        }
+        if (!permissionList.isEmpty()) {
+            String[] permission = permissionList.toArray(new String[permissionList.size()]);
+            ActivityCompat.requestPermissions(getActivity(), permission, 1);
+        } else {
+            requestLocation();
+        }
         return view;
+    }
+
+
+    private LocationClientOption initLocation() {
+        LocationClientOption option = new LocationClientOption();
+        option.setIsNeedAddress(true);
+        option.setScanSpan(5000);
+        option.setCoorType("bd09ll");
+        option.setIsNeedLocationDescribe(true);
+        option.setNeedDeviceDirect(false);
+        option.setOpenGps(true);
+        option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy);
+        return option;
+    }
+    private void requestLocation(){
+        LocationClientOption option = initLocation();
+        mLocationClient.setLocOption(option);
+        MyLocationListener myLocationListener = new MyLocationListener();
+        mLocationClient.registerLocationListener(myLocationListener);
+        mLocationClient.start();
     }
 
     private void initShangjias(){
@@ -129,13 +188,47 @@ public class ShouyeFragment extends Fragment {
     @Override
     public void onDestroy() {
         super.onDestroy();
+        mLocationClient.stop();
         mapView.onDestroy();
+        baiduMap.setMyLocationEnabled(false);
+        mapView = null;
     }
 
     @Override
     public void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
     }
+
+    private void navigateTo(BDLocation location) {
+        if (isFirstLocate) {
+            LatLng ll = new LatLng(location.getLatitude(), location.getLongitude());
+            MapStatusUpdate update = MapStatusUpdateFactory.newLatLng(ll);
+            baiduMap.animateMapStatus(update);
+            update = MapStatusUpdateFactory.zoomTo(16f);
+            baiduMap.animateMapStatus(update);
+            isFirstLocate = false;
+        }
+        MyLocationData.Builder locationBuilder = new MyLocationData.Builder();
+        locationBuilder.latitude(location.getLatitude());
+        locationBuilder.longitude(location.getLongitude());
+        MyLocationData locationData = new MyLocationData.Builder()
+                .accuracy(location.getRadius())
+                .direction(location.getDirection()).latitude(location.getLatitude())
+                .longitude(location.getLongitude()).build();
+        baiduMap.setMyLocationData(locationData);
+    }
+
+    public class MyLocationListener extends BDAbstractLocationListener{
+        @Override
+        public void onReceiveLocation(BDLocation location) {
+            if (location.getLocType() == BDLocation.TypeGpsLocation || location.getLocType() == BDLocation.TypeNetWorkLocation) {
+                navigateTo(location);
+            }
+            TextView dizhitext = (TextView) getActivity().findViewById(R.id.dizhitext);
+            dizhitext.setText("当前位置: "+location.getLocationDescribe());
+        }
+    }
+
     public static int getStatusBarHeight(Context context) {
         Resources resources = context.getResources();
         int resourceId = resources.getIdentifier("status_bar_height", "dimen", "android");
@@ -144,3 +237,4 @@ public class ShouyeFragment extends Fragment {
     }
 
 }
+
