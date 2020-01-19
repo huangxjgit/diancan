26a8944527d7e2479e4590c1afe4e7c492de1bdf
huangxjgit <2250016889@qq.com>
2020-01-19 17:22:39 +0800

    评价页面碎片实现

--- app/src/main/java/com/example/diancan/DindanFragment.java
+++ app/src/main/java/com/example/diancan/DindanFragment.java
@@ -4,19 +4,50 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class DindanFragment extends Fragment {
+import com.example.diancan.tabfragment.Tab1Fragment;
+import com.example.diancan.tabfragment.Tab2Fragment;
+import com.example.diancan.tabfragment.Tab3Fragment;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class DindanFragment extends Fragment {
+    private View view;
+    private String[] titles = {"全部订单", "待付款", "待评价"};
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    //写一个List集合，把每个页面，也就是Fragment,存进去
+    private List<Fragment> list;
+    private MyAdapter adapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.dindan, container, false);
+        view = inflater.inflate(R.layout.fragment_dindan, container, false);
+        //实例化
+        viewPager = (ViewPager) view.findViewById(R.id.viewpager);
+        tabLayout = (TabLayout) view.findViewById(R.id.tablayout);
+        //页面，数据源，里面是创建的三个页面（Fragment）
+        list = new ArrayList<>();
+        list.add(new Tab1Fragment());
+        list.add(new Tab2Fragment());
+        list.add(new Tab3Fragment());
+        //ViewPager的适配器，获得Fragment管理器
+        adapter = new MyAdapter(getActivity().getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
+        //将TabLayout和ViewPager绑定在一起，一个动另一个也会跟着动
+        tabLayout.setupWithViewPager(viewPager);
+        return view;
     }
 
     @Override
@@ -37,6 +68,27 @@ public class DindanFragment extends Fragment {
     @Override
     public void onDestroy() {
         super.onDestroy();
+    }
+    class MyAdapter extends FragmentPagerAdapter {
+
+        public MyAdapter(FragmentManager fm) {
+            super(fm);
+        }
+        //获得每个页面的下标
+        @Override
+        public Fragment getItem(int position) {
+            return list.get(position);
+        }
+        //获得List的大小
+        @Override
+        public int getCount() {
+            return list.size();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return titles[position];
+        }
     }
 
 }
---
+++ app/src/main/java/com/example/diancan/tabfragment/Tab3Fragment.java
@@ -0,0 +1,39 @@
+package com.example.diancan.tabfragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.diancan.R;
+
+public class Tab3Fragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_tab3, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+}
